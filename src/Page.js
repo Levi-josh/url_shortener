816e9dac6f3343672cc986f24e08ab91232d59b3
Levi-josh <levijoshuakelly@gmail.com>
2023-10-10 08:40:45 -0200

    second commit

--- src/Page.js
+++ src/Page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaBars, FaChevronCircleDown, FaChevronCircleUp, FaCopyright, FaLink, FaPaintBrush, FaSortAlphaDown, FaSortDown } from "react-icons/fa"
+import { FaBars, FaChartBar, FaChevronCircleDown, FaChevronCircleUp, FaCopyright, FaLink, FaPaintBrush, FaSortAlphaDown, FaSortDown } from "react-icons/fa"
 
 
 export default function Page() {
@@ -35,7 +35,7 @@ export default function Page() {
   }
   return (
     <div className='w-full'>
-      <header className='bg-blue-600 flex justify-between items-center h-10 text text-white sm:h-11 md:h-14 xl:h-16'>
+      <header className='bg-blue-600 flex justify-between items-center h-12 text text-white sm:h-14 md:h-14 xl:h-16 fixed w-full '>
         <div className='  border-white border-r-2 h-full flex items-center w-20 sm:w-24 md: w-30 lg:w-40'>
 
           <h2 className='pl-3   border-white  sm:pl-5  md:pl-7 text-lg  lg:font-bold  lg:pl-16'>weblify</h2>
@@ -53,14 +53,14 @@ export default function Page() {
       </header>
       <section>
 
-        <div className=' bg-newblue h-100 pt-3 text-center pb-3 sm:pt-5 lg:pt-8 xl:pt-12 md:pt-7'>
-          <div className='lg:flex justify-center gap-2'>  <h1 className='text-lg font-bold lg:text-xl xl:text-2xl'>Effortless URL Shortening For </h1><span className='text-lg font-bold text-blue-600 lg:text-xl xl:text-2xl '>Everyone</span></div>
-          <p className=' text-md text-black font-semibold md:pt-1 lg:pt-4 md:text-lg xl:text-xl'>Effortlessly create short links for social media,marketing and more</p>
+        <div className=' bg-newblue h-100 pt-20 text-center pb-3 sm:pt-24 lg:pt-28 xl:pt-32 md:pt-25 px-3'>
+          <div className=' lg:flex justify-center lg:gap-2'>  <h1 className='text-lg font-bold lg:text-xl xl:text-2xl'>Effortless URL Shortening For </h1><span className='text-lg font-bold text-blue-600 lg:text-xl xl:text-2xl '>Everyone</span></div>
+          <p className='pt-1  text-md text-black font-semibold md:pt-1 lg:pt-4 md:text-lg xl:text-xl'>Effortlessly create short links for social media,marketing and more</p>
           <div className='flex  justify-center flex-col md:flex-row lg:px-72  md:px-44 lg:gap-10'>
             <input type='text' placeholder='Enter your long link here' className=' rounded-md outline-none m-auto pl-2 mt-4 h-7 w-80 placeholder:text-black sm:w-96 md:h-8 lg:h-9 lg:w-full ' />
             <button className='bg-blue-600 w-24 font-semibold m-auto mt-3 h-7 rounded-lg text-sm text-white md:mt-4 lg:mt-4 lg:h-8 lg:w-28'>shorten url</button>
           </div>
-          <p className='mt-4 text-sm md:text-base md:mb-6 lg:mb-8 lg:text-lg'>By using this our url shortener you agree to our terms and condition</p>
+          <p className='mt-2 text-sm md:text-base md:mt-6 md:mb-6 lg:mb-8 lg:text-lg'>By using this our url shortener you agree to our terms and condition</p>
         </div>
 
         <p className='text-center mt-5 text-lg mb-5 sm:mb-10 sm:mt-7 lg:text-xl lg:mt-8 lg:mb-8'>Why <span className='font-bold'>weblify</span>?</p>
@@ -76,16 +76,9 @@ export default function Page() {
             <p className='text-sm mt-4 sm:text-base lg:text-lg xl:text-xl'>Weblify offers A "<span>Custom Link</span>solution" for users to craft and customize their short links.</p>
           </div>
           <div className=' bg-newblue w-64 h-72 rounded-xl text-center pt-10 px-5  lg:w-72 lg:h-96 sm:w-72 sm:h-80 '>
-            <div className='bg-black w-10 h-10 rounded-full flex justify-center items-center m-auto sm:w-12 sm:h-12 lg:w-16 lg:h-16 md:h-14 md:w-14'><svg className='text-lg text-white lg:text-xl' width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g id="streamline:money-graph-bar-product-data-bars-analysis-analytics-graph-business-chart" clip-path="url(#clip0_404_117)">
-                <path id="Vector" d="M13.5 13.5H0.5V0.5H4.5V13.5V6.5H8.5V13.5V3.5H12.5V13.5" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
-              </g>
-              <defs>
-                <clipPath id="clip0_404_117">
-                  <rect width="14" height="14" fill="white" />
-                </clipPath>
-              </defs>
-            </svg></div>
+            <div className='bg-black w-10 h-10 rounded-full flex justify-center items-center m-auto sm:w-12 sm:h-12 lg:w-16 lg:h-16 md:h-14 md:w-14'><FaChartBar className='text-xl text-white lg:text-2xl'>
+
+            </FaChartBar></div>
             <h1 className='font-bold mt-3 lg:text-lg xl:text-xl'>Track your clicks</h1>
             <p className='text-sm mt-4  md:mt-1 sm:text-base lg:text-lg xl:text-xl'>Weblify is an advanced link analytics platform that <span>tracks clicks</span>on short links and provides extensive statistics to help you measure the effectiveness of your short links</p>
           </div>
@@ -98,27 +91,27 @@ export default function Page() {
           </div>
           <div className='mx-5 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-24 '>
             <div className='mt-10'>
-              <div className='flex justify-between  pb-2'>  <p className='text-base font-medium sm:text-base md:text-lg'>What is a URL shortener?</p>{showinfo1 ? <FaChevronCircleDown onClick={displayinfo1} /> : <FaChevronCircleUp onClick={displayinfo1} />}</div>
+              <div className='flex justify-between  pb-2'>  <p className='text-base font-medium sm:text-base md:text-lg'>What is a URL shortener?</p>{showinfo1 ? <FaChevronCircleDown onClick={displayinfo1} className=' sm:text-base lg:text-xl' /> : <FaChevronCircleUp onClick={displayinfo1} className='sm:text-base lg:text-xl' />}</div>
               <div className='border-b-2 border-black overflow-hidden '><p className={`xl:text-lg mb-2 ${showinfo1 ? '-translate-y-20 hidden ' : '-translate-y- 20'}`}>A url shortener is used in making the url of a website short and simple</p></div>
             </div>
             <div>
-              <div className='flex justify-between  pb-2 mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>How do i use the URL shortener?</p>{showinfo2 ? <FaChevronCircleDown onClick={displayinfo2} /> : <FaChevronCircleUp onClick={displayinfo2} />}</div>
+              <div className='flex justify-between  pb-2 mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>How do i use the URL shortener?</p>{showinfo2 ? <FaChevronCircleDown onClick={displayinfo2} className=' sm:text-base lg:text-xl' /> : <FaChevronCircleUp onClick={displayinfo2} className=' sm:text-base lg:text-xl' />}</div>
               <div className='border-b-2 border-black'><p className={`xl:text-lg mb-2 ${showinfo2 ? '-translate-y-20 hidden ' : '-translate-y- 20'}`} >its simple all you have to do is paste in your long link and press the shorten button</p></div>
             </div>
             <div>
-              <div className='flex justify-between  pb-2  mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>Can i customize my short links?</p>{showinfo3 ? <FaChevronCircleDown onClick={displayinfo3} /> : <FaChevronCircleUp onClick={displayinfo3} />}</div>
+              <div className='flex justify-between  pb-2  mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>Can i customize my short links?</p>{showinfo3 ? <FaChevronCircleDown onClick={displayinfo3} className='sm:text-base lg:text-xl' /> : <FaChevronCircleUp onClick={displayinfo3} className='sm:text-base lg:text-xl' />}</div>
               <div className='border-b-2 border-black'><p className={`xl:text-lg mb-2 ${showinfo3 ? '-translate-y-20 hidden ' : '-translate-y- 20'}`}>yes,you can customize your shortened links,all you have to do is to ..............</p></div>
             </div>
             <div>
-              <div className='flex justify-between  pb-2  mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>Are there analytics available for my short links?</p>{showinfo4 ? <FaChevronCircleDown onClick={displayinfo4} /> : <FaChevronCircleUp onClick={displayinfo4} />}</div>
+              <div className='flex justify-between  pb-2  mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>Are there analytics available for my short links?</p>{showinfo4 ? <FaChevronCircleDown onClick={displayinfo4} className='sm:text-base lg:text-xl' /> : <FaChevronCircleUp onClick={displayinfo4} className=' sm:text-base lg:text-xl' />}</div>
               <div className='border-b-2 border-black'><p className={`xl:text-lg mb-2 ${showinfo4 ? '-translate-y-20 hidden ' : '-translate-y- 20'}`} >yes there are analytics available for your short links,i dont even know what is analytics.......</p></div>
             </div>
             <div>
-              <div className='flex justify-between  pb-2  mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>Is there a limit to the number of links i can shorten?</p>{showinfo5 ? <FaChevronCircleDown onClick={displayinfo5} /> : <FaChevronCircleUp onClick={displayinfo5} />}</div>
+              <div className='flex justify-between  pb-2  mt-2 gap-4'> <p className='text-base font-medium sm:text-base md:text-lg'>Is there a limit to the number of links i can shorten?</p>{showinfo5 ? <FaChevronCircleDown onClick={displayinfo5} className='text-lg sm:text-base lg:text-xl' /> : <FaChevronCircleUp onClick={displayinfo5} className='text-lg sm:text-base lg:text-xl' />}</div>
               <div className='border-b-2 border-black'><p className={`xl:text-lg mb-2 ${showinfo5 ? '-translate-y-20 hidden ' : '-translate-y- 20'}`} >no, you can shorten your links as many as you want my dear ........................</p></div>
             </div>
             <div>
-              <div className='flex justify-between pb-2  mt-2'> <p className='text-base font-medium sm:text-base md:text-lg'>How long are my shorts links generated by your platform?</p>{showinfo6 ? <FaChevronCircleDown onClick={displayinfo6} /> : <FaChevronCircleUp onClick={displayinfo6} />}</div>
+              <div className='flex justify-between pb-2  mt-2 gap-4'> <p className='text-base font-medium sm:text-base md:text-lg'>How long are my shorts links generated by your platform?</p>{showinfo6 ? <FaChevronCircleDown onClick={displayinfo6} className='text-xl sm:text-base lg:text-xl' /> : <FaChevronCircleUp onClick={displayinfo6} className='text-xl sm:text-base lg:text-xl' />}</div>
               <div className='border-b-2 border-black'><p className={`xl:text-lg mb-2 ${showinfo6 ? '-translate-y-20 hidden ' : '-translate-y- 20'}`} >just few manys with a strong connection your shortened links are ready</p></div>
             </div>
           </div>
